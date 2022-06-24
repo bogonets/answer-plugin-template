b1acf377f28ab1228daee305cad2893cfa663eb0
zer0 <osom8979@gmail.com>
2022-06-24 10:06:20 +0900

    Create ReccPlugin class

--- fe/src/recc/index.ts
+++ fe/src/recc/index.ts
@@ -1,7 +1,25 @@
 import Vue from 'vue';
+import type {PluginObject} from 'vue/types/plugin';
+import type {ReccCwcPluginOptions} from '@recc/api/dist/reccCwcPlugin';
 import Recc from '@/recc/recc';
 
 const recc = new Recc();
-Vue.use(recc);
+
+class ReccPlugin implements PluginObject<ReccCwcPluginOptions>
+{
+  static installed = false;
+
+  install(vue: typeof Vue) {
+    if (ReccPlugin.installed) {
+      console.warn('Already installed recc plugin');
+      return;
+    }
+
+    ReccPlugin.installed = true;
+    vue.prototype.$recc = recc;
+  }
+}
+
+Vue.use(new ReccPlugin());
 
 export default recc;
--- fe/src/recc/recc.ts
+++ fe/src/recc/recc.ts
@@ -1,26 +1,9 @@
-import VueInterface from 'vue';
-import type {PluginObject} from 'vue/types/plugin';
 import type {ReccCwcPluginOptions} from '@recc/api/dist/reccCwcPlugin';
 import {ReccCwcPlugin} from '@recc/api/dist/reccCwcPlugin';
 
-export default class Recc
-  extends ReccCwcPlugin
-  implements PluginObject<ReccCwcPluginOptions>
-{
+export default class Recc extends ReccCwcPlugin {
   constructor(options?: ReccCwcPluginOptions) {
     super(window, options ?? {origin: window.origin});
-  }
-
-  private static installed = false;
-
-  install(Vue: typeof VueInterface) {
-    if (Recc.installed) {
-      console.warn('Already installed Recc plugin');
-      return;
-    }
-
-    Recc.installed = true;
-    Vue.prototype.$recc = this;
   }
 
   // TODO: More APIs
